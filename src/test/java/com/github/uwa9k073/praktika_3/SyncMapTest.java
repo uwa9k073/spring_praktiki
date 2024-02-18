fc9dcbe7b92a033077b73fd1420ff64955623080
uwa9k073 <112890512+uwa9k073@users.noreply.github.com>
2024-02-18 14:15:20 +0300

    Обновление классов (#7)
    * feat: add sync map and sync list
    
    Tests: протестировано ci
    Relates: #5
    
    * feat: add simple unit tests
    
    Tests: протестировано ci
    Relates: #5
    
    * minor
    
    * solve conflict
    
    * refatcor code style

--- src/test/java/com/github/uwa9k073/praktika_3/SyncMapTest.java
+++ src/test/java/com/github/uwa9k073/praktika_3/SyncMapTest.java
@@ -10,12 +10,15 @@ class SyncMapTest {
   @Test
   void put() {
     SyncMap<String, Integer> map = new SyncMap<>();
+
     Thread thread1 = new Thread(()->map.put("key1", 1));
     Thread thread2 = new Thread(()->map.put("key2", 2));
+
     thread1.start();
     thread2.start();
     thread1.join();
     thread2.join();
+
     assertEquals(1, map.get("key1"));
     assertEquals(2, map.get("key2"));
   }
@@ -24,12 +27,15 @@ class SyncMapTest {
   @Test
   void remove() {
     SyncMap<String, Integer> map = new SyncMap<>();
+
     Thread thread1 = new Thread(()->map.put("key1", 1));
     Thread thread2 = new Thread(()->map.remove("key1"));
+
     thread1.start();
     thread2.start();
     thread1.join();
     thread2.join();
+
     assertEquals(null, map.get("key1"));
   }
 }
