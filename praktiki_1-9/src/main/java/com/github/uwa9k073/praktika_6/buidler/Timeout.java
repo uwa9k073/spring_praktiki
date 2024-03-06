841ab9a03fd5c9a84dd8206d99dc03779ce19ffc
Константин Орлов <konsor-79@mail.ru>
2024-03-06 20:17:43 +0300

    feat: add tests for builder
    Tests: протестировано ci
    Relates: #13

--- praktiki_1-9/src/main/java/com/github/uwa9k073/praktika_6/buidler/Timeout.java
+++ praktiki_1-9/src/main/java/com/github/uwa9k073/praktika_6/buidler/Timeout.java
@@ -2,11 +2,13 @@ package com.github.uwa9k073.praktika_6.buidler;
 
 import java.util.concurrent.TimeUnit;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 
 
 @Getter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class Timeout {
   private Integer amount;
   private TimeUnit measure;
---
+++ praktiki_1-9/src/test/java/com/github/uwa9k073/praktika_6/buidler/DefaultRequestBuilderTest.java
@@ -0,0 +1,42 @@
+package com.github.uwa9k073.praktika_6.buidler;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.jupiter.api.BeforeEach;
+
+class RequestBuilderTest {
+
+  private RequestBuilder requestBuilder;
+
+  @BeforeEach
+  void setup(){
+    this.requestBuilder = Request.builder();
+  }
+
+
+  void Test_Get_Method(){
+    var request = requestBuilder.get("url").build();
+
+    assertEquals("url", request.getUrl());
+    assertEquals(Method.GET, request.getMethod());
+  }
+
+
+  void Test_POST_Method(){
+    var request = requestBuilder.post("url").data("some data").build();
+
+    assertEquals("url", request.getUrl());
+    assertEquals(Method.POST, request.getMethod());
+    assertEquals("some data", request.getData().get());
+  }
+
+
+  void Test_Settings_Setup(){
+    var request = requestBuilder.get("url").timeout(2, TimeUnit.SECONDS).retry(5).build();
+
+    assertEquals(new Timeout(2, TimeUnit.SECONDS), request.getTimeout());
+    assertEquals(5, request.getRetries());
+  }
+
+}
