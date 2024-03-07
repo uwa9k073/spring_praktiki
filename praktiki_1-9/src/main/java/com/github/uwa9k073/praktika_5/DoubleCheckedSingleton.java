0f15b2fb78a1c16a4786757fc3e0e7e15f770430
Константин Орлов <konsor-79@mail.ru>
2024-03-07 09:38:04 +0300

    feat: improve singletones

--- praktiki_1-9/src/main/java/com/github/uwa9k073/praktika_5/DoubleCheckedSingleton.java
+++ praktiki_1-9/src/main/java/com/github/uwa9k073/praktika_5/DoubleCheckedSingleton.java
@@ -1,9 +1,10 @@
 package com.github.uwa9k073.praktika_5;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class DoubleCheckedSingleton {
   private static volatile DoubleCheckedSingleton instance;
-
-  private DoubleCheckedSingleton(){}
 
   public static DoubleCheckedSingleton getInstance(){
     if (instance==null){
--- praktiki_1-9/src/main/java/com/github/uwa9k073/praktika_5/EagerSingleton.java
+++ praktiki_1-9/src/main/java/com/github/uwa9k073/praktika_5/EagerSingleton.java
@@ -1,9 +1,10 @@
 package com.github.uwa9k073.praktika_5;
 
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
 public class EagerSingleton {
   private static final  EagerSingleton INSTANCE = new EagerSingleton();
-
-  private EagerSingleton(){}
 
   public static synchronized EagerSingleton getInstance(){
     return INSTANCE;
