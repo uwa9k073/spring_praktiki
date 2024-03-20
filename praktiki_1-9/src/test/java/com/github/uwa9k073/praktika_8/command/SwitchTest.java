ced91164c2dcc7f8829a6cc02631aaf37fdb9405
Константин Орлов <konsor-79@mail.ru>
2024-03-21 00:07:21 +0300

    refactor: rebase command pattern

--- praktiki_1-9/src/test/java/com/github/uwa9k073/praktika_8/command/SwitchTest.java
+++ praktiki_1-9/src/test/java/com/github/uwa9k073/praktika_8/command/SwitchTest.java
@@ -1,6 +1,6 @@
 package com.github.uwa9k073.praktika_8.command;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -9,7 +9,7 @@ class SwitchTest {
   @Test
   void flip() {
     Turn light = new Light();
-    Switch flip = new Switch(new TurnOnLightCommand<>(light), new TurnOffCommand<>(light));
+    Switch flip = new Switch(new TurnOnCommand<>(light), new TurnOffCommand<>(light));
     assertEquals(Light.turnOnMessage, flip.flipUp());
     assertEquals(Light.turnOffMessage, flip.flipDown());
   }
